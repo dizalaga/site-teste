381f58f913fb1c37234d75ed7aefbceaa2038f31
DizAlaga <152033851+dizalaga@users.noreply.github.com>
2023-12-08 01:40:32 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import "./components/QuemSomos/index.css";
 import "./components/FormPost/FormPost.css";
 import "./components/FormPost/PostList.css";
 import "./components/Footer/Footer.css";
+import "./components/Usuario/usuario.css";
 
-import Navbar from "./components/navbar/index.js";
+import Navbar from "./components/navbar/index";
 import Home from "./components/Home/index";
-import Login from "./components/Login/index.js";
-import CadastroForm from "./components/Cadastro/index.js";
-import PontosDoacao from "./components/PontosDoacao/index.js";
-import QuemSomos from "./components/QuemSomos/index.js";
+import Login from "./components/Login/index";
+import CadastroForm from "./components/Cadastro/index";
+import PontosDoacao from "./components/PontosDoacao/index";
+import QuemSomos from "./components/QuemSomos/index";
 import FormPost from "./components/FormPost/FormPost";
 import PostList from "./components/FormPost/PostList";
 import Footer from "./components/Footer/Footer";
 import AreaRiscos from "./components/AreaRiscos/areaRiscos";
+import Usuario from "./components/Usuario/usuario";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -35,6 +37,7 @@ function App() {
         <Route path="/areas-de-risco" element={<AreaRiscos />} />
         <Route path="/pontos-de-doacao" element={<PontosDoacao />} />
         <Route path="/quemsomos" element={<QuemSomos />} />
+        <Route path="/usuario" element={<Usuario />} />
       </Routes>
     </Router>
   );
