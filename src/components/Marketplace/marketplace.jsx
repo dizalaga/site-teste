a404e3d9d6bb17a2a25f10a9fd811adc3e1cb4b1
thaisvictoria <142370586+thaisvictoria@users.noreply.github.com>
2023-12-09 22:18:01 -0300

    Update marketplace.jsx

--- src/components/Marketplace/marketplace.jsx
+++ src/components/Marketplace/marketplace.jsx
@@ -1,111 +1,148 @@
 import React from "react";
+import camaUsada from "./imagens-marketplace/1.jpg";
+import Armario from "./imagens-marketplace/2.jpg";
+import seguroPorto from "./imagens-marketplace/3.jpg";
+import seguroLiberty from "./imagens-marketplace/4.jpg";
+import seguroAllianz from "./imagens-marketplace/5.png";
+import sofáUsado from "./imagens-marketplace/6.jpeg";
 import "./marketplace.css";
-import Navbar from "../navbar";
-import Footer from "../Footer/Footer";
 
-import p1 from "./imagens-marketplace/1.jpg";
-import p2 from "./imagens-marketplace/2.jpg";
-import p3 from "./imagens-marketplace/3.jpg";
-import p4 from "./imagens-marketplace/4.jpg";
-import p5 from "./imagens-marketplace/5.png";
-import p6 from "./imagens-marketplace/6.jpeg";
-import ProductCard from "./productcard";
+import Navbar from "../navbar/index.jsx";
+import Footer from "../Footer/Footer";
+import { Link } from "react-router-dom";
 
 function Marketplace() {
-  const products = [
-    {
-      name: "Cama Usada",
-      title: "Cama Usada",
-      price: "R$250.00",
-      image: p1,
-      description:
-        "Cama antiga e usada tamanho king-size. Espaçosa e sem danos no bairro da Madalena.",
-    },
-
-    {
-      name: "Armario Semiusado",
-      title: "Armario Semiusado",
-      price: "R$340.00",
-      image: p2,
-      description:
-        "Armário recém comprado e em perfeito estado. Já foi usado, mas ainda está completamente novo",
-    },
-
-    {
-      name: "Seguro Residencial - Porto Seguro",
-      title: "Seguro Residencial - Porto Seguro",
-      price: "R$200.00",
-      image: p3,
-      description:
-        "Assine já um plano de seguro para previnir perca de bens em acidentes.",
-    },
-
-    {
-      name: "Seguro Residencial - Liberty Seguros",
-      title: "Seguro Residencial - Liberty Seguros",
-      price: "R$215.00",
-      image: p4,
-      description:
-        "Assine já um plano de seguro para previnir perca de bens em acidentes.",
-    },
-
-    {
-      name: "Seguro Residencial - Allianz",
-      title: "Seguro Residencial - Allianz",
-      price: "R$210.00",
-      image: p5,
-      description:
-        "Assine já um plano de seguro para previnir perca de bens em acidentes.",
-    },
-
-    {
-      name: "Sofá Usado",
-      title: "Sofá Usado",
-      price: "R$400.00",
-      image: p6,
-      description:
-        "Sofá usado e com algumas marcas do tempo, porém ainda grande e espaçoso",
-    },
-  ];
-
-  const handleBuyClick = (productId) => {
-    console.log(`Comprando produto ${productId}`);
-  };
-
-  const handleSaveClick = (productId) => {
-    console.log(`Salvando produto ${productId}`);
-  };
-
-  const productsTop = products.slice(0, 3);
-  const productsBottom = products.slice(3);
-
   return (
     <div>
       <span className="navvv">
         <Navbar></Navbar>
       </span>
       <div className="container-marketplace">
-        <h2 className="titleMarketplace">Marketplace</h2>
+        <h2 className="nameTitle">Marketplace</h2>
         <div className="product-section">
-          {productsTop.map((product) => (
-            <ProductCard
-              key={product.name}
-              product={product}
-              onBuyClick={handleBuyClick}
-              onSaveClick={handleSaveClick}
+          <div className="card">
+            <img src={camaUsada} alt="Cama Usada" className="imageProduct" />
+            <h3 className="nameTitleCama">Cama Usada</h3>
+            <p className="descriptionCama">
+              Cama antiga e usada tamanho king-size. Espaçosa e sem danos no
+              bairro da Madalena.
+            </p>
+            <p className="location">Recife-PE</p>
+            <p className="price">R$250.00</p>
+            <div className="button-container">
+              <Link to="https://wa.me/818633710">
+                <button className="contato">Entrar em contato</button>
+              </Link>
+            </div>
+          </div>
+
+          <div className="card">
+            <img
+              src={Armario}
+              alt="Armario Semiusado"
+              className="imageProduct"
             />
-          ))}
-        </div>
-        <div className="product-section">
-          {productsBottom.map((product) => (
-            <ProductCard
-              key={product.name}
-              product={product}
-              onBuyClick={handleBuyClick}
-              onSaveClick={handleSaveClick}
+            <h3 className="nameTitleArmario">Armario Semiusado</h3>
+            <p className="descriptionArmario">
+              Armário recém comprado e em perfeito estado. Já foi usado, mas
+              ainda está completamente novo
+            </p>
+            <p className="location">Recife-PE</p>
+            <p className="price">R$240.00</p>
+            <div className="button-container">
+              <Link to="https://wa.me/818633710">
+                <button className="contato">Entrar em contato</button>
+              </Link>
+            </div>
+          </div>
+
+          <div className="card">
+            <img
+              src={seguroPorto}
+              alt="Seguro Residencial - Porto Seguro"
+              className="imageProduct"
             />
-          ))}
+            <h3 className="nameTitlePortoSeguro">
+              Seguro Residencial - Porto Seguro
+            </h3>
+            <p className="descriptionPortoSeguro">
+              Assine já um plano de seguro para previnir perca de bens em
+              acidentes.
+            </p>
+            <p className="invisivel">lalal</p>
+            <p className="location">Recife-PE</p>
+            <p className="price">R$200.00</p>
+            <div className="button-container">
+              <Link to="#">
+                <button className="saibaMais">Saiba Mais</button>
+              </Link>
+            </div>
+          </div>
+
+          <div className="card">
+            <img
+              src={seguroLiberty}
+              alt="Seguro Residencial - Liberty Seguros"
+              className="imageProduct"
+            />
+            <h3 className="nameTitleLibertySeguro">
+              CSeguro Residencial - Liberty Seguros
+            </h3>
+            <p className="descriptionLibertySeguro">
+              Assine já um plano de seguro para previnir perca de bens em
+              acidentes.
+            </p>
+            <p className="location">Recife-PE</p>
+            <p className="price">R$215.00</p>
+            <div className="button-container">
+              <Link to="#">
+                <button className="saibaMais">Saiba Mais</button>
+              </Link>
+            </div>
+          </div>
+
+          <div className="card">
+            <img
+              src={seguroAllianz}
+              alt="Seguro Residencial - Allianz"
+              className="imageProduct"
+            />
+            <h3 className="nameTitleAllianz">Seguro Residencial - Allianz</h3>
+            <p className="descriptionAllianz">
+              Assine já um plano de seguro para previnir perca de bens em
+              acidentes.
+            </p>
+            <p className="location">Recife-PE</p>
+            <p className="price">R$210.00</p>
+            <div className="button-container">
+              <Link to="#">
+                <button className="saibaMais">Saiba Mais</button>
+              </Link>
+            </div>
+          </div>
+
+          <div className="card">
+            <img src={sofáUsado} alt="Sofá Usado" className="imageProduct" />
+            <h3 className="nameTitleSofa">Sofá Usado</h3>
+            <p className="descriptionSofa">
+              Sofá usado e com algumas marcas do tempo, porém ainda grande e
+              espaçoso
+            </p>
+            <p className="location">Recife-PE</p>
+            <p className="price">R$400.00</p>
+            <div className="button-container">
+              <Link to="https://wa.me/818633710">
+                <button className="contato">Entra em contato</button>
+              </Link>
+            </div>
+          </div>
         </div>
+        <Link to="/marketplace" className="paginas">
+          <p className="tracinho">{"<"}</p>
+          <p className="um">1 </p> 
+          <p className="tracinho">{">"} </p>
+          
+        </Link>
       </div>
       <span className="footer">
         <Footer></Footer>
